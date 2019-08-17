0ebabd784df82404ec83700fbf7355e8ca1b7327
kremerdesign <kremerdesign@gmail.com>
2019-08-17 00:38:28 -0700

    Add Feedback Component

--- src/App.js
+++ src/App.js
@@ -3,44 +3,61 @@ import React, { Component } from "react";
 import './App.css';
 import {DEFAULT_LATITUDE, DEFAULT_LONGITUDE} from "./config/Constants";
 import {findCoordinates} from "./utilities/GetLocation";
+import {Feedback} from "./Feedback";
+import EarthquakeDetail from "./EarthquakeDetail";
 
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {location: {latitude: DEFAULT_LATITUDE, longitude: DEFAULT_LONGITUDE}};
-
+    this.state = {
+      location: {
+        latitude: DEFAULT_LATITUDE,
+        longitude: DEFAULT_LONGITUDE
+      },
+      feedbackText: "Calculating",
+    };
     this.findCoordinates = findCoordinates.bind(this);
+    this.findCoordinates()
+
   }
 
 
 
 
     componentDidMount() {
-      this.findCoordinates()
+    // trigger function to calculate the rest
     }
 
 
 render() {
-  const status = this.state.location.longitude;
+  const status = this.state.location.longitude.toFixed(4);
 
   return (
     <div className="App">
       <header className="App-header">
         {/*<Header />*/}
+        <h3>Did you feel something?</h3>
+        <Feedback value={"It was probably nothing, ok!"}/>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           {status}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          + {status}
-        </a>
+
       </header>
+      {/* Feedback box */}
+      <h1> Recent Nearby Earthquakes </h1>
+
+      <div> Map goes here </div>
+      {/*  Main Map */}
+
+      <h1> Nearest Earthquake data: </h1>
+      {/* Multi maps */}
+      <div> little maps go here </div>
+      <ol id={"list"} className={"tilted-list"} >
+        <EarthquakeDetail value={"iidk"} />
+        <EarthquakeDetail value={"iidk"} />
+      </ol>
 
 
     </div>
---
+++ src/Feedback.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Feedback(props) {
+  return (
+    <div className="feedback" id={"feel"} >
+        <h1> {props.value} </h1>
+    </div>
+  );
+}
