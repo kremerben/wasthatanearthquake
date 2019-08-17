ee03329e37208e2b146c3fd9ef85358f0cc2bdc8
kremerdesign <kremerdesign@gmail.com>
2019-08-17 00:40:00 -0700

    Stub in EarthquakeDetail component

---
+++ src/EarthquakeDetail.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+
+
+class EarthquakeDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {location: "helloworld"};
+
+  }
+
+
+
+
+  componentDidMount() {
+  }
+
+
+render() {
+
+  return (
+    <li className="viewmap"  >
+        <h1> description here </h1>
+
+    </li>
+  );}
+}
+
+export default EarthquakeDetail;
+
