f396e1df13bcbcbbe0817ece1d1e76adeb7fd8bc
kremerdesign <kremerdesign@gmail.com>
2019-08-19 22:18:25 -0700

    Add Earthquake detail component

--- src/EarthquakeDetail.js
+++ src/EarthquakeDetail.js
@@ -1,30 +1,46 @@
 import React, { Component } from "react";
-
+import {GOOGLE_MAPS_API_KEY} from "./config/Constants";
 
 
 class EarthquakeDetail extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {location: "helloworld"};
-
-  }
-
-
-
-
-  componentDidMount() {
-  }
+    constructor(props) {
+        super(props);
+    }
 
 
 render() {
+    let data = this.props.value.properties;
+    let totalMinutesAgo = Math.floor((Date.now() - data.time) / 60000);
+    let hourAgo = Math.floor(totalMinutesAgo / 60);
+    let minutesAgo = (totalMinutesAgo % 60).toFixed();
 
-  return (
-    <li className="viewmap"  >
-        <h1> description here </h1>
+    let coords = this.props.value.geometry;
+    let depth = coords.coordinates[2];
+    let eq_longitude = coords.coordinates[0];
+    let eq_latitude = coords.coordinates[1];
+    let distanceFrom = Math.round(coords.distanceFromYou);
 
+    let eqDetail = hourAgo ? `${hourAgo} hours and ` : "";
+    eqDetail += `${minutesAgo} minutes ago, located ${distanceFrom} km from you, \n`;
+    eqDetail += `${data.place}, at a depth of ${depth} km, and a Magnitude of ${data.strength}.`;
+
+    let googleMapImg = `https://maps.googleapis.com/maps/api/staticmap?zoom=7&center=${eq_latitude},${eq_longitude}&markers=color:red|label:1|${eq_latitude},${eq_longitude}&size=380x200&maptype=roadmap&key=${GOOGLE_MAPS_API_KEY}`;
+    let googleMapStyle = {
+        display: "none",
+        opacity: 1,
+    };
+
+    return (
+    <li className="viewmap">
+        <h3>{eqDetail}</h3>
+        <button>Toggle map</button>
+        <div style={googleMapStyle} className="maps1">
+            <a href={data.equrl} target="_blank">
+                <img src={googleMapImg} />
+            </a>
+        </div>
     </li>
-  );}
+    );}
 }
 
 export default EarthquakeDetail;
-
--- src/config/Constants.js
+++ src/config/Constants.js
@@ -8,3 +8,4 @@ export const USGS_HOUR_URL = USGS_BASE_URL + "1.0_hour.geojson";
 export const USGS_DAY_URL = USGS_BASE_URL + "1.0_day.geojson";
 export const USGS_WEEK_URL = USGS_BASE_URL + "1.0_week.geojson";
 
+export const GOOGLE_MAPS_API_KEY = "AIzaSyDy-JqylpwpC21oUjNRkuCt-5mPSx8uZhs";
