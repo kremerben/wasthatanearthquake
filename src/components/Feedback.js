7af0799e81db575ebe77ddd32a6a66956003672a
kremerdesign <kremerdesign@gmail.com>
2019-08-27 00:22:23 -0700

    Simplify feedback component

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export function Feedback(props) {
-    let feedbackText = (!props.value) ? "Oh! That might have been something!" : "It was probably nothing.";
+    let feedbackText = (props.value) ? "Oh! That might have been something!" : "It was probably nothing.";
     return (
         <div className={"feedback"} id={"feel"} >
             <h1> {feedbackText} </h1>
